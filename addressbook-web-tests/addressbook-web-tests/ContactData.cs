517754398780600ba2b8ef1feebb41023e38997e
axeg <alikahad92@gmail.com>
2019-11-05 01:08:01 +0300

    added new test for adding contacts
    added ContactDataCreation  test and ContactData class
    removed needless files

---
+++ addressbook-web-tests/addressbook-web-tests/ContactData.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebaddressbookTests
+{
+    class ContactData
+    {
+        private string firstName;
+        private string middleName;
+        private string lastName;
+        private string nickname;
+        private string email;
+
+        public ContactData(string firstName, string middleName)
+        {
+            this.firstName = firstName;
+            this.middleName = middleName;
+        }
+
+        public ContactData(string firstName, string middleName,string lastName, string nickname, string email)
+        {
+            this.firstName = firstName;
+            this.middleName = middleName;
+            this.lastName = lastName;
+            this.nickname = nickname;
+            this.email = email;
+        }
+
+        public string FirstName
+        {
+            get => firstName;
+
+            set => firstName = value;
+        }
+
+        public string MiddleName
+        {
+            get => middleName;
+
+            set => middleName = value;
+        }
+
+        public string LastName
+        {
+            get => lastName;
+
+            set => lastName = value;
+        }
+
+        public string Email
+        {
+            get => email;
+
+            set => email = value;
+        }
+
+        public string Nickname
+        {
+            get => firstName;
+
+            set => firstName = value;
+        }
+    }
+}
