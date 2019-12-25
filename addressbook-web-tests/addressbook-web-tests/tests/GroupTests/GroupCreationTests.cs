dfcb035033515848f0b2ad8439be57102db1c69f
axeg <amcustomer01@gmail.com>
2019-12-25 14:44:22 +0300

    csv 6.1

--- addressbook-web-tests/addressbook-web-tests/tests/GroupTests/GroupCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/GroupTests/GroupCreationTests.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace WebaddressbookTests
 {
@@ -22,7 +23,22 @@ namespace WebaddressbookTests
             return groups;
         }
 
-        [Test, TestCaseSource("RandomGroupDataProvider")]
+        public static IEnumerable<GroupData> GroupDataFromFile()
+        {
+            List<GroupData> groups = new List<GroupData>();
+            string[] lines = File.ReadAllLines(@"groups.csv");
+            foreach (string l in lines)
+            {
+                string[] parts = l.Split(',');
+                groups.Add(new GroupData(parts[0]){
+                    Header = parts[1],
+                    Footer = parts[2]
+                });
+            }
+            return groups;
+        }
+
+        [Test, TestCaseSource("GroupDataFromFile")]
         public void GroupCreationTest(GroupData group)
         {
             List<GroupData> oldGroups = app.Groups.GetGroupList();
