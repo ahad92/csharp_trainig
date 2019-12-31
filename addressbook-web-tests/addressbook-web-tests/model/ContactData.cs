874564b177763edc1d850dc1a2ab28914212461a
axeg <amcustomer01@gmail.com>
2019-12-31 15:10:04 +0300

    added constructor in contact data

--- addressbook-web-tests/addressbook-web-tests/model/ContactData.cs
+++ addressbook-web-tests/addressbook-web-tests/model/ContactData.cs
@@ -15,6 +15,10 @@ namespace WebaddressbookTests
             LastName = lastName;
         }
 
+        public ContactData()
+        {
+        }
+
         public ContactData(string v)
         {
         }
