57c04bfac67a8106445bb25186ab9ca18d5d7224
axeg <alikahad92@gmail.com>
2020-01-03 03:04:55 +0300

    improvements

--- addressbook-web-tests/addressbook-web-tests/tests/ContactRemovalTestsFromGroup.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/ContactRemovalTestsFromGroup.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
-namespace addressbook_web_tests.tests
+namespace WebaddressbookTests
 {
-    class ContactRemovalTestsFromGroup
+
+    public class RemovingContactFromGroupTests : AuthTestBase
     {
+        [Test]
+        public void TestRemovingContactFromGroup()
+        {
+            if (!app.Groups.IsGroupExist())
+            {
+                app.Groups.Create(new GroupData("test1"));
+            }
+
+            if (!app.Contacts.IsContactExist())
+            {
+                app.Contacts.Create(new ContactData("Firstname", "Lastname"));
+            }
+
+            GroupData group = GroupData.GetAll()[0];
+
+            app.Contacts.AddAllContactsToGroup(group);
+
+            List<ContactData> oldList = group.GetContacts();
+            ContactData contact = ContactData.GetAll()[0];
+
+            app.Contacts.RemoveContactFromGroup(contact, group);
+
+            List<ContactData> newList = group.GetContacts();
+            oldList.Remove(contact);
+            oldList.Sort();
+            newList.Sort();
+            Assert.AreEqual(oldList, newList);
+        }
     }
 }
