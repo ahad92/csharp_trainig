4c5d96e93b7c98e7d66892400ea8890c8bcec743
axeg <alikahad92@gmail.com>
2020-01-03 03:01:57 +0300

    added test for add and deleting contact from groups

--- addressbook-web-tests/addressbook-web-tests/tests/AddContactToGroupTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/AddContactToGroupTests.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,18 +8,26 @@ using NUnit.Framework;
 namespace WebaddressbookTests
 {
 
-    public class AddingContactToGroupTests : AuthTestBase
+    public class AddContactToGroupTests : AuthTestBase
     {
         [Test]
-        public void TestAddingContactToGroup()
+        public void AddContactToGroup()
         {
+            if (!app.Groups.IsGroupExist())
+            {
+                app.Groups.Create(new GroupData("test1"));
+            }
+
+            if (!app.Contacts.IsContactExist())
+            {
+                app.Contacts.Create(new ContactData("Firstname","Lastname"));
+            }
+
             GroupData group = GroupData.GetAll()[0];
             List<ContactData> oldList = group.GetContacts();
-            ContactData contact = ContactData.GetAll().Except(oldList).First();
+            ContactData contact = ContactData.GetAll()[0];
 
-            //actions
             app.Contacts.AddContactToGroup(contact, group);
-
             List<ContactData> newList = group.GetContacts();
             oldList.Add(contact);
             oldList.Sort();
