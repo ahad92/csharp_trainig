ab692cb6fc79ba5676b9bc981095a68050cb1cbe
axeg <alikahad92@gmail.com>
2020-01-07 00:46:14 +0300

    Задание №18: Реализовать тесты для добавления и удаления групп

---
+++ addressbook_tests_autoit/tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace addressbook_tests_autoit
+{
+    class UnitTest1 : TestBase
+    {
+        [Test]
+
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
