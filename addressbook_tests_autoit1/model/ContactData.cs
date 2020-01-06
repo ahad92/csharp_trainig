876448e2917cf2c41b2a434ea20b14eb46652d50
axeg <alikahad92@gmail.com>
2020-01-07 02:04:21 +0300

    Задание №18-19: Реализовать тесты для доб и удаления групп и контактов

---
+++ addressbook_tests_autoit1/model/ContactData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_tests_autoit
+{
+    public class ContactData : IComparable<ContactData>, IEquatable<ContactData>
+    {
+        public string FirstName { get; set; }
+
+        public int CompareTo(ContactData other)
+        {
+            return this.FirstName.CompareTo(other.FirstName);
+        }
+
+        public bool Equals(ContactData other)
+        {
+            return this.FirstName.Equals(other.FirstName);
+        }
+    }
+}
