3dc4e384235c5df16f6e627b75cfb1e9a1faa2ed
axeg <alikahad92@gmail.com>
2020-01-07 00:46:14 +0300

    Задание №18-19: Реализовать тесты для доб и удаления групп и контактов

---
+++ addressbook_tests_autoit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace addressbook_tests_autoit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ addressbook_tests_autoit/tests/ContactRemovalTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class ContactRemovalTests : TestBase
+    {
+        [Test]
+        public void TestContactRemoval()
+        {
+            List<ContactData> oldContacts = app.Contacts.GetContactList();
+
+            if (oldContacts.Count == 0)
+            {
+                app.Contacts.Add(new ContactData() { FirstName = "New Contact" });
+                oldContacts = app.Contacts.GetContactList();
+            }
+
+            app.Contacts.Delete(0);
+
+            List<ContactData> newContacts = app.Contacts.GetContactList();
+            oldContacts.RemoveAt(0);
+            oldContacts.Sort();
+            newContacts.Sort();
+            Assert.AreEqual(oldContacts, newContacts);
+        }
+    }
+}
